dc1326f4b5beabea6e5250e0eb2d4a9494923cb3
GODARA7 <subhashgodara237057@gmail.com>
2024-12-04 23:59:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,27 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
 import os
 
-API_ID = API_ID =  26512964
-
-API_HASH = os.environ.get("API_HASH", "e5d187c6c7a0919ccb8866f76f655701")
-
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7916334119:AAFAsGnwdtO_o8NQbMFR-iHbzkwObyHvWLI")
-
-PASS_DB = int(os.environ.get("PASS_DB", "mongodb+srv://sujay5372192:sujay5372192@cluster00001.zivqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster00001"))
-
-OWNER = int(os.environ.get("OWNER", 7246728595))
-
-LOG = -4679498951,
-
-# UPDATE_GRP = -4679498951, # bot sat group
-
-# auth_chats = [7246728595]
-
-try:
-    ADMINS=[]
-    for x in (os.environ.get("ADMINS", "7246728595").split()):
-        ADMINS.append(int(x))
-except ValueError:
-        raise Exception("Your Admins list does not contain valid integers.")
-ADMINS.append(OWNER)
-
-
+class Config(object):
+    # get a token from @BotFather
+    pass
+    """
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7916334119:AAFAsGnwdtO_o8NQbMFR-iHbzkwObyHvWLI")
+    API_ID = int(os.environ["API_ID", 26512964]
+    API_HASH = os.environ["API_HASH", "e5d187c6c7a0919ccb8866f76f655701"]
+    AUTH_USERS = "7246728595"
+# 
